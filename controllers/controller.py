5740df3c7465ce382bdb0f6bc8a1b4824b599152
Joshua <jssmiller94@gmail.com>
2021-04-18 15:38:33 +0100

    about to begin testing the random function i entered in player.py

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,7 +1,18 @@
 from flask import render_template, request
 from app import app
+from models.src.game import Game
+from models.src.player import Player, computer_choice
 
-@app.route('/')
+
+@app.route('/rps_home')
 def index():
-    render_template('index.html', title='Home')
+    return render_template('index.html', title='Home')
+
+@app.route('/rps_home/<player_1>/<player_2>', methods=['POST'])
+def play_game(player_1, player_2):
+    player_1_name = request.form['name']
+    player_1_move = request.form['move']
+    player_1 = Player(player_1_name, player_1_move)
+    player_2 = Player("Bleep-Bloop", computer_choice)
+    return Game.decide_winner(player_1, player_2)
 
--- models/src/player.py
+++ models/src/player.py
@@ -1,3 +1,9 @@
+import random
+
+choices = ["Rock", "Paper", "Scissors"]
+computer_choice = choices[random.randint(0, len(choices)-1)]
+
+
 class Player():
 
     def __init__(self, name, move):
